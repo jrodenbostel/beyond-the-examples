f4517846f06fc1de147bc94456fbc15d8463de8d
Justin Rodenbostel <justin@rodenbostel.com>
2014-05-12 07:27:04 -0500

    simple object binding

--- part-3/src/main/java/com/rodenbostel/sample/SampleController.java
+++ part-3/src/main/java/com/rodenbostel/sample/SampleController.java
@@ -3,6 +3,7 @@ package com.rodenbostel.sample;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class SampleController {
@@ -10,6 +11,13 @@ public class SampleController {
     @RequestMapping("/")
     public String index(Model model) {
         model.addAttribute("gizmo", new Gizmo());
+        return "hello";
+    }
+
+    @RequestMapping("/save")
+    public String save(Gizmo gizmo) {
+        System.out.println(gizmo.getField1());
+        System.out.println(gizmo.getField2());
         return "hello";
     }
 }
