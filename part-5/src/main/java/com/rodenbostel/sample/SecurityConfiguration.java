8b27e57d691e09737c6a0082eee302ecca47ee90
Justin Rodenbostel <justin@rodenbostel.com>
2014-05-28 07:55:16 -0500

    to the database

--- part-5/src/main/java/com/rodenbostel/sample/SecurityConfiguration.java
+++ part-5/src/main/java/com/rodenbostel/sample/SecurityConfiguration.java
@@ -1,6 +1,7 @@
 package com.rodenbostel.sample;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -21,6 +22,29 @@ import javax.sql.DataSource;
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+    @Value("${spring.datasource.driverClassName}")
+    private String databaseDriverClassName;
+
+    @Value("${spring.datasource.url}")
+    private String datasourceUrl;
+
+    @Value("${spring.datasource.username}")
+    private String databaseUsername;
+
+    @Value("${spring.datasource.password}")
+    private String databasePassword;
+
+    @Bean
+    public DataSource datasource() {
+        org.apache.tomcat.jdbc.pool.DataSource ds = new org.apache.tomcat.jdbc.pool.DataSource();
+        ds.setDriverClassName(databaseDriverClassName);
+        ds.setUrl(datasourceUrl);
+        ds.setUsername(databaseUsername);
+        ds.setPassword(databasePassword);
+
+        return ds;
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
@@ -37,6 +61,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().withUser("user").password("password").roles("USER");
+        auth.jdbcAuthentication().dataSource(datasource()).withDefaultSchema().withUser("user").password("password").roles("USER");
     }
 }
